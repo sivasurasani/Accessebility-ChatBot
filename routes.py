9825e0659b66f05bfe9754267ec178ddcfe7ff03
Deepak <deepakadimoolam@my.unt.edu>
2023-09-19 21:38:39 -0500

    Rule based tokenizers commented

--- routes.py
+++ routes.py
@@ -1,14 +1,14 @@
-import tensorflow as tf
+#import tensorflow as tf
+import sqlite3
 from flask import Flask, request, jsonify, render_template, url_for, flash, redirect
-from train import trainModel
+#from train import trainModel
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from werkzeug.exceptions import abort
 
 SESSION_TYPE = 'memcache'
+
 app = Flask(__name__)
-
-
 
 responses = {
     "hello": "Hi there! How can I assist you?",
@@ -21,31 +21,31 @@ tokenizer = Tokenizer()
 tokenizer.fit_on_texts(responses.keys())
 
 def chatbot_response(user_input):
-    user_input_seq = tokenizer.texts_to_sequences([user_input])[0]
-    max_similarity = 0
-    best_response = "I don't understand that."
+   user_input_seq = tokenizer.texts_to_sequences([user_input])[0]
+   max_similarity = 0
+   best_response = "I don't understand that."
 
-    for word, response in responses.items():
-        word_seq = tokenizer.texts_to_sequences([word])[0]
-        similarity = len(set(user_input_seq).intersection(word_seq))
+   for word, response in responses.items():
+       word_seq = tokenizer.texts_to_sequences([word])[0]
+       similarity = len(set(user_input_seq).intersection(word_seq))
 
-        if user_input == word:
-            return response
-        if similarity > max_similarity:
-            max_similarity = similarity
-            best_response = response
+       if user_input == word:
+           return response
+       if similarity > max_similarity:
+           max_similarity = similarity
+           best_response = response
 
-    if max_similarity == len(user_input_seq):
-        return best_response
+       if max_similarity == len(user_input_seq):
+           return best_response
 
-    return "I don't understand that."
+   return "I don't understand that."
 
-while True:
-    user_input = input("You: ")
-    if user_input.lower() == 'exit':
-        break
-    response = chatbot_response(user_input.lower())
-    print("Chatbot:", response)
+#while True:
+    #user_input = input("You: ")
+#    if user_input.lower() == 'exit':
+#        break
+#    response = chatbot_response(user_input.lower())
+#    print("Chatbot:", response)
 
 
 
