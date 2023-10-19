f0b5177bd9b549cfdaa80220068a231c5e86ab55
Siva Kumar Reddy Surasani <surasanisiva1729@gmail.com>
2023-10-19 16:09:28 -0500

    Chatbot logic changes

--- chatbot_v3.py
+++ chatbot_v3.py
@@ -11,12 +11,32 @@ tfidf_matrix = tfidf_vectorizer.fit_transform(data['Questions'])
 
 def chatbot_response_v3(user_input):
     user_input = user_input.lower()
-    user_vector = tfidf_vectorizer.transform([user_input])
-    cosine_similarities = cosine_similarity(user_vector, tfidf_matrix)
-    best_match_index = cosine_similarities.argmax()
-    response = data.loc[best_match_index, 'Answers']
-    response = response.split(":")
-    output = {
-            "response" : response[1]
+    if(user_input in ('hi', 'hello', 'hi!', 'hello!')):
+        output = {
+            "response" : 'Hello! How can I help you?'
         }
+    elif(user_input in ('how are you','how are you?')):
+        output = {
+            "response" : 'Hello! I am fine! How can I help you?'
+        }
+    elif(user_input in ('who are you', 'who are you?')):
+        output = {
+            "response" : 'I am an accessibility chatbot and I am here to help with mobile accessibility guidelines'
+        }
+    else:
+        user_vector = tfidf_vectorizer.transform([user_input])
+        cosine_similarities = cosine_similarity(user_vector, tfidf_matrix)
+        best_match_index = cosine_similarities.argmax()
+        matching_percentage = cosine_similarities[0][best_match_index] * 100
+        response = data.loc[best_match_index, 'Answers']
+        # response = response.split(":")
+        if(matching_percentage > 0.5):
+            output = {
+                "response" : response
+            }
+        else:
+            output = {
+                "response" : "I don't understand that!"
+            }
+   
     return output
